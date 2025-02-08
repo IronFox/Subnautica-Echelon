732fec27567bff7522368dec6aec2763cfef0879
IronFox <github@portal.delta-works.org>
2025-02-08 10:03:06 +0100

    Fix cockpit wall positioning. Add enable-flag for screen

--- Assets/Behavior/Components/ScreenControl.cs
+++ Assets/Behavior/Components/ScreenControl.cs
@@ -20,6 +20,11 @@ public class ScreenControl : MonoBehaviour
     private RenderTexture depthTexture;
     public float screenDistance = 0.5f;
     private Mesh screenQuad;
+
+    private int originalCullingMask = -1;
+
+    public bool isEnabled;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,52 +42,66 @@ public class ScreenControl : MonoBehaviour
 
         int w = Camera.main.pixelWidth;
         int h = Camera.main.pixelHeight;
-        if (w != lastWidth || h != lastHeight)
+        if (isEnabled)
         {
-            if (colorTexture != null)
+            if (w != lastWidth || h != lastHeight)
             {
-                Destroy(colorTexture);
-                Destroy(depthTexture);
-            }
-            Debug.Log($"Creating new textures at size {w}*{h}");
-            lastWidth = w;
-            lastHeight = h;
-            colorTexture = new RenderTexture(new RenderTextureDescriptor(w, h, RenderTextureFormat.ARGBHalf, 24));
-            depthTexture = new RenderTexture(new RenderTextureDescriptor(w, h, RenderTextureFormat.RFloat, 24));
-            trailingColorCamera.depthTextureMode = DepthTextureMode.Depth;
-            trailingColorCamera.targetTexture = colorTexture;
-            //Camera.main.depthTextureMode = DepthTextureMode.Depth;
-            trailingColorCamera.enabled = true;
-            trailingColorCamera.fieldOfView = Camera.main.fieldOfView;
+                if (colorTexture != null)
+                {
+                    Destroy(colorTexture);
+                    Destroy(depthTexture);
+                }
+                Debug.Log($"Creating new textures at size {w}*{h}");
+                lastWidth = w;
+                lastHeight = h;
+                colorTexture = new RenderTexture(new RenderTextureDescriptor(w, h, RenderTextureFormat.ARGBHalf, 24));
+                depthTexture = new RenderTexture(new RenderTextureDescriptor(w, h, RenderTextureFormat.RFloat, 24));
+                trailingColorCamera.depthTextureMode = DepthTextureMode.Depth;
+                trailingColorCamera.targetTexture = colorTexture;
+                //Camera.main.depthTextureMode = DepthTextureMode.Depth;
+                trailingColorCamera.fieldOfView = Camera.main.fieldOfView;
 
-            screenMaterial.SetTexture("_Color", colorTexture);
-            screenMaterial.SetTexture("_Depth", depthTexture);
-            screenMaterial.SetFloat("_PixelSizeX", 1f / w);
-            screenMaterial.SetFloat("_PixelSizeY", 1f / h);
-            screenMaterial.SetFloat("_PixelAspect", (float)w/h);
+                screenMaterial.SetTexture("_Color", colorTexture);
+                screenMaterial.SetTexture("_Depth", depthTexture);
+                screenMaterial.SetFloat("_PixelSizeX", 1f / w);
+                screenMaterial.SetFloat("_PixelSizeY", 1f / h);
+                screenMaterial.SetFloat("_PixelAspect", (float)w / h);
 
 
-            float screenHeightAtOne = 2 * Mathf.Tan(Camera.main.fieldOfView / 2 * Mathf.PI / 180);
+                float screenHeightAtOne = 2 * Mathf.Tan(Camera.main.fieldOfView / 2 * Mathf.PI / 180);
                 //Camera.main.fieldOfView / 90f;
-            float screenHeight = screenHeightAtOne * screenDistance;
+                float screenHeight = screenHeightAtOne * screenDistance;
 
-            var roomSizeY = screenHeight;// * 0.98f;    //make a little smaller to see room
-            var roomSizeX = (float)w / h * roomSizeY;
-            var wallFull = 0.05f;
-            var wall = wallFull /2;
-            transform.localScale = new Vector3(roomSizeX, roomSizeY, 1);
-            leftRoomWall.localScale = new Vector3(wallFull, roomSizeY, 1);
-            leftRoomWall.position = new Vector3(roomSizeX/2 + wall, 0, 0);
-            rightRoomWall.localScale = new Vector3(wallFull, roomSizeY, 1);
-            rightRoomWall.position = new Vector3(-roomSizeX/2 - wall, 0, 0);
-            roomCeiling.localScale = new Vector3(wallFull, roomSizeX + wallFull, 1);
-            roomCeiling.position = new Vector3(0, roomSizeY / 2 + wall, 0);
-            roomFloor.localScale = new Vector3(wallFull, roomSizeX + wallFull, 1);
-            roomFloor.position = new Vector3(0, -roomSizeY / 2 - wall, 0);
-            rearRoomWall.transform.localScale = new Vector3(wallFull, roomSizeY + wallFull, roomSizeX + wallFull);
+                var roomSizeY = screenHeight;// * 0.98f;    //make a little smaller to see room
+                var roomSizeX = (float)w / h * roomSizeY;
+                var wallFull = 0.05f;
+                var wall = wallFull / 2;
+                transform.localScale = new Vector3(roomSizeX, roomSizeY, 1);
+                leftRoomWall.localScale = new Vector3(wallFull, roomSizeY, 1);
+                leftRoomWall.localPosition = new Vector3(roomSizeX / 2 + wall, 0, 0);
+                rightRoomWall.localScale = new Vector3(wallFull, roomSizeY, 1);
+                rightRoomWall.localPosition = new Vector3(-roomSizeX / 2 - wall, 0, 0);
+                roomCeiling.localScale = new Vector3(wallFull, roomSizeX + wallFull, 1);
+                roomCeiling.localPosition = new Vector3(0, roomSizeY / 2 + wall, 0);
+                roomFloor.localScale = new Vector3(wallFull, roomSizeX + wallFull, 1);
+                roomFloor.localPosition = new Vector3(0, -roomSizeY / 2 - wall, 0);
+                rearRoomWall.transform.localScale = new Vector3(wallFull, roomSizeY + wallFull, roomSizeX + wallFull);
 
+            }
+            if (!trailingColorCamera.enabled)
+            {
+                originalCullingMask = Camera.main.cullingMask;
+                Debug.Log($"old culling mask: {Camera.main.cullingMask}");
+                Camera.main.cullingMask = ~1;
+                Debug.Log($"new culling mask: {Camera.main.cullingMask}");
+            }
+            trailingColorCamera.enabled = true;
         }
-
+        else
+        {
+            trailingColorCamera.enabled = false;
+            Camera.main.cullingMask = originalCullingMask;
+        }
 
 
     }
