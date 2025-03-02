206497548915025fbd56732882172d805deaef89
IronFox <github@portal.delta-works.org>
2025-03-02 10:53:36 +0100

    Provide example assemble and bake scripts

---
+++ Scripts/assemble echelon.bat
@@ -0,0 +1,9 @@
+set "installPath=G:\SteamLibrary\steamapps\common\Subnautica"
+set "buildPath=..\..\BuildTarget"
+
+copy /Y "%buildPath%\Subnautica Echelon_Data\Managed\EchelonScripts.dll" "%installPath%\BepInEx\plugins\Echelon"
+copy /Y "..\Unity\Assets\AssetBundles\Windows\echelon" "%installPath%\BepInEx\plugins\Echelon"
+copy /Y "..\Unity\Assets\AssetBundles\OSX\echelon" "%installPath%\BepInEx\plugins\Echelon\echelon.osx"
+copy /Y "..\Plugin\bin\Release\net4.7.2\Subnautica Echelon.dll" "%installPath%\BepInEx\plugins\Echelon"
+mkdir "%installPath%\BepInEx\plugins\Echelon\images"
+copy /Y "..\images\*.*" "%installPath%\BepInEx\plugins\Echelon\images"
---
+++ Scripts/bake.bat
@@ -0,0 +1,13 @@
+set "buildPath=..\..\BuildTarget"
+
+del /Q echelon.zip
+rmdir /Q /S .\Echelon
+mkdir .\Echelon
+mkdir .\Echelon\images
+copy /Y "%buildPath%\Subnautica Echelon_Data\Managed\EchelonScripts.dll" .\Echelon
+copy /Y "..\Unity\Assets\AssetBundles\Windows\echelon" .\Echelon
+copy /Y "..\Unity\Assets\AssetBundles\OSX\echelon" .\Echelon\echelon.osx
+copy /Y "..\Plugin\bin\Release\net4.7.2\Subnautica Echelon.dll" .\Echelon
+copy /Y "..\images\*.*" .\Echelon\images
+
+powershell Compress-Archive .\Echelon echelon.zip
