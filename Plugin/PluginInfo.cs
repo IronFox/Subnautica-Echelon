467edcda028ecb842400ef8ff302067ebd3662c2
IronFox <github@portal.delta-works.org>
2025-04-03 10:33:22 +0200

    Increment version

--- Plugin/PluginInfo.cs
+++ Plugin/PluginInfo.cs
@@ -10,6 +10,6 @@ namespace Subnautica_Echelon
     {
         public const string PLUGIN_GUID = "com.ironfox.subnautica.echelon.mod";
         public const string PLUGIN_NAME = "Echelon";
-        public const string PLUGIN_VERSION = "1.2.7";
+        public const string PLUGIN_VERSION = "1.2.8";
     }
 }
