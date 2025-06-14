ae998e8b3aa0ea69e367d6dc5c4b3c132d6cb823
IronFox <github@portal.delta-works.org>
2025-06-14 13:07:12 +0200

    Derive max shake time from intensity and distance

--- EchelonUnity/Assets/Behavior/Components/CameraShake.cs
+++ EchelonUnity/Assets/Behavior/Components/CameraShake.cs
@@ -20,12 +20,10 @@ public class CameraShake : MonoBehaviour
             && x.Origin == Origin.RailgunCharge);
         if (existing != null)
             existing.Terminate();
-        shakeEvents.Add(new ShakeEvent(Origin.RailgunFire, shot, 0.01f)
+        shakeEvents.Add(new ShakeEvent(Origin.RailgunFire, shot, 0.01f, maxDistance: null)
         {
-            radius = 100,
-            speed = 15,
-            intensity = mark * mark * 0.15f,
-            maxAge = 1f,
+            frequency = 15,
+            intensity = mark * mark * 0.25f
         });
     }
 
@@ -38,23 +36,19 @@ public class CameraShake : MonoBehaviour
     {
         if (!shakeEvents.Any(x => x.Target == shot))
         {
-            shakeEvents.Add(new ShakeEvent(Origin.RailgunCharge, shot, 1f)
+            shakeEvents.Add(new ShakeEvent(Origin.RailgunCharge, shot, 1f, infiniteAge: true, maxDistance: null)
             {
-                radius = 100,
-                speed = 10,
-                intensity = mark * mark * 0.02f,
-                maxAge = 100,
+                frequency = 10,
+                intensity = mark * 0.03f,
             });
         }
     }
 
     public void SignalTorpedoFired()
     {
-        shakeEvents.Add(new ShakeEvent(Origin.TorpedoFire, echelon.transform, 0.1f)
+        shakeEvents.Add(new ShakeEvent(Origin.TorpedoFire, echelon.transform, 0.1f, maxDistance: null)
         {
-            radius = 100,
             intensity = 0.06f,
-            maxAge = 0.5f,
         });
     }
 
@@ -69,13 +63,13 @@ public class CameraShake : MonoBehaviour
         var maxDistance = explosionRadius * 10;
         if (distance > maxDistance)
             return;
-        var proximity = (ExplosionD(distance) - ExplosionD(maxDistance)) / (ExplosionD(0) - ExplosionD(maxDistance));
-        shakeEvents.Add(new ShakeEvent(Origin.Explosion, explosion, 0.01f)
+        //var proximity = (ExplosionD(distance) - ExplosionD(maxDistance)) / (ExplosionD(0) - ExplosionD(maxDistance));
+        shakeEvents.Add(new ShakeEvent(Origin.Explosion, explosion, 0.01f, maxDistance, timeScale: 0.7f)
         {
-            radius = maxDistance,
-            speed = 20,
-            intensity = 0.5f,
-            maxAge = 1.5f * proximity,
+            //radius = maxDistance,
+            frequency = 20,
+            intensity = 0.8f,
+            //maxAge = 1.5f * proximity,
         });
     }
 
@@ -92,12 +86,10 @@ public class CameraShake : MonoBehaviour
         {
             if (overdriveEvent == null)
             {
-                overdriveEvent = new ShakeEvent(Origin.Overdrive, echelon.transform, 2f)
+                overdriveEvent = new ShakeEvent(Origin.Overdrive, echelon.transform, fadeIn: 2f, maxDistance: null, infiniteAge: true)
                 {
-                    radius = 10000,
-                    speed = 5 * overdriveIntensity.Value,
+                    frequency = 5 * overdriveIntensity.Value,
                     intensity = 0.02f * BoostScale * overdriveIntensity.Value,
-                    maxAge = 100000
                 };
                 shakeEvents.Add(overdriveEvent);
             }
@@ -115,7 +107,7 @@ public class CameraShake : MonoBehaviour
         Vector2 intensity = Vector3.zero;
         foreach (var ev in shakeEvents)
         {
-            ev.age += Time.deltaTime;
+            ev.Advance();
             if (!ev.Target)
             {
                 ev.Log("Target expired");
@@ -135,12 +127,10 @@ public class CameraShake : MonoBehaviour
 
     internal void SignalCollision(Collision collision)
     {
-        shakeEvents.Add(new ShakeEvent(Origin.Collision, echelon.transform, 0.01f)
+        shakeEvents.Add(new ShakeEvent(Origin.Collision, echelon.transform, fadeIn: 0.01f, maxDistance: 20)
         {
-            intensity = collision.impulse.magnitude * 0.0000025f,
-            speed = 20,
-            maxAge = 0.8f
-
+            intensity = collision.impulse.magnitude * 0.0000075f,
+            frequency = 20,
         });
     }
 
@@ -157,30 +147,56 @@ public class CameraShake : MonoBehaviour
     class ShakeEvent
     {
         public Origin Origin { get; }
-        public float speed = 20;
-        public float age;
+        public float frequency = 20;
+        private float scaledAge;
+        private float absoluteAge;
         public float intensity = 1;
-        public float radius = 100;
-        public float maxAge = 1;
+        private float? maxDistance;
+        //public float radius = 100;
+        //public float maxAge = 1;
         private bool terminal;
         private float terminalSince;
         public float FadeIn { get; }
+        public bool InfiniteDistance => !maxDistance.HasValue;
+        public bool InfiniteAge { get; }
+        public float TimeScale { get; }
         public Transform Target { get; }
         private Vector3 lastLocation;
         private Vector2 _Seed = Random.insideUnitCircle * 10;
 
-        public bool ShouldRemove => age > maxAge
-            || (terminal && age > terminalSince + 1);
+        public bool ShouldRemove => scaledAge > 1
+            || (terminal && absoluteAge > terminalSince + 1);
 
 
         private static float TimeFalloff(float x)
         {
-            return 1f / (1f + 0.1f * x);
+            return 1f / (1f + 10f * x);
         }
-        private float Falloff => (TimeFalloff(age) - TimeFalloff(maxAge)) / (TimeFalloff(0) - TimeFalloff(maxAge));
+        private float Falloff => (TimeFalloff(scaledAge) - TimeFalloff(1)) / (TimeFalloff(0) - TimeFalloff(1));
+        private static float DistanceFalloff(float x)
+        {
+            return 1f / (1f + 0.01f * x * x);
+        }
+        private float DFalloff => (DistanceFalloff(Distance) - DistanceFalloff(maxDistance.Value)) / (DistanceFalloff(0) - DistanceFalloff(maxDistance.Value));
+
+        private float Distance => M.Distance(lastLocation, LastCamera);
+        public Vector3 LastCamera { get; private set; }
+
         public void Log(string message)
         {
             //Debug.Log($"[{Origin}]<{Target}>@{age} {message} ");
+        }
+
+        public void Advance()
+        {
+            absoluteAge += Time.deltaTime;
+            if (!InfiniteAge && !InfiniteDistance)
+            {
+                float scale = 0.05f / (DFalloff * this.intensity);
+                scaledAge += scale * Time.deltaTime * TimeScale;
+            }
+            else if (!InfiniteAge)
+                scaledAge += Time.deltaTime * TimeScale;
         }
 
         public void Terminate()
@@ -188,34 +204,41 @@ public class CameraShake : MonoBehaviour
             if (!terminal)
             {
                 terminal = true;
-                terminalSince = age;
+                terminalSince = absoluteAge;
                 Log("Terminating");
             }
         }
-        public ShakeEvent(Origin type, Transform target, float fadeIn)
+        public ShakeEvent(Origin type, Transform target, float fadeIn, float? maxDistance, bool infiniteAge = false, float timeScale = 1)
         {
             Origin = type;
             Target = target;
             FadeIn = fadeIn;
+            this.maxDistance = maxDistance;
+            InfiniteAge = infiniteAge;
+            TimeScale = timeScale;
             Log("Created");
         }
         public Vector2 GetShake(Vector3 camera)
         {
             if (Target)
                 lastLocation = Target.position;
-
+            LastCamera = camera;
             var magnitude =
-                (1f - M.Smoothstep(0, radius, M.Distance(lastLocation, camera)))
-                * Falloff
-                * M.Smoothstep(0, FadeIn, age)
+                Falloff
+                * M.Smoothstep(0, FadeIn, absoluteAge)
                 * intensity;
+
+            if (maxDistance.HasValue)
+                magnitude *= (1f - M.Smoothstep(0, maxDistance.Value, M.Distance(lastLocation, camera)));
+
+
             if (terminal)
-                magnitude *= 1f - M.Smoothstep(0, 1, (age - terminalSince));
+                magnitude *= 1f - M.Smoothstep(0, 1, (absoluteAge - terminalSince));
 
-            var x = Mathf.Sin(speed * (_Seed.x + _Seed.y + Time.time));
-            var y = Mathf.Cos(speed * (_Seed.x * Mathf.PI + _Seed.y * 2.132f + Time.time * 1.0234f));
+            var x = Mathf.Sin(frequency * (_Seed.x + _Seed.y + Time.time));
+            var y = Mathf.Cos(frequency * (_Seed.x * Mathf.PI + _Seed.y * 2.132f + Time.time * 1.0234f));
 
-            var time = Time.time;
+            var time = absoluteAge;
             var noise = new Vector2(
                     Mathf.PerlinNoise(_Seed.x, time * 0.5f) - 0.5f,
                     Mathf.PerlinNoise(_Seed.y, time * 0.5f) - 0.5f
